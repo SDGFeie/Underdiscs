9c09b9d0bdfafa9ade72fb1916cceaa4bcd78343
SDGFeie <sdgfeie@gmail.com>
2026-05-20 11:08:04 +0200

    Base project structure

---
+++ src/main/java/net/sdgfeie/underdiscs/UnderdiscsClient.java
@@ -0,0 +1,11 @@
+package net.sdgfeie.underdiscs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnderdiscsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
